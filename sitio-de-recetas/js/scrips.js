2a2ca93211560a88bdbfd5e2e17d3e8d87153fca
Antonela Menna <147436512+Antonelanm@users.noreply.github.com>
2024-07-10 14:43:40 -0300

    Add files via upload

---
+++ sitio-de-recetas/js/scrips.js
@@ -0,0 +1,12 @@
+.jumbotron {
+    background-image: url('../images/banner.jpg');
+    background-size: cover;
+    background-position: center;
+    color: white;
+    text-align: center;
+}
+
+.card img {
+    height: 200px;
+    object-fit: cover;
+}
